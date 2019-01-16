bed2c5716ca8024239610bd44a578f56ec437910
Thomas Bruckmaier (Xubuntu Laptop) <thomasbruckmaier@web.de>
2019-01-16 14:09:01 +0100

    Added documentation

--- src/Models/Generic.php
+++ src/Models/Generic.php
@@ -2,8 +2,15 @@
 
 namespace Corcel\Acf\Models;
 
+/**
+ * Fallback field for all unknown fields. Returns the plain value stored in the
+ * post_meta table
+ */
 class Generic extends BaseField
 {
+    /**
+     * @return boolean
+     */
     public function getValueAttribute()
     {
         return $this->internal_value;
