807c288402e8eee557b4e0c88c51eee4348bc829
Thomas Bruckmaier (Xubuntu Laptop) <thomasbruckmaier@web.de>
2019-01-23 20:58:23 +0100

    code style

--- src/Models/FlexibleContent.php
+++ src/Models/FlexibleContent.php
@@ -46,7 +46,7 @@ class FlexibleContent extends BaseField
         foreach ($this->internal_value as $i => $contentBlockType) {
             $block = $this->layout_blocks->get($contentBlockType)->keyBy('post_excerpt');
 
-            $block = $block->map(function ($field) use ($i){
+            $block = $block->map(function ($field) use ($i) {
                 $internalName = sprintf('%s_%d_%s', $this->localKey, $i, $field->post_excerpt);
                 return (clone $field)->setData($this->data)->setLocalKey($internalName);
             });
