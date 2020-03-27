262e3d34c73407a122826a35c5289f9decf158a7
Thomas Bruckmaier (Xubuntu Laptop) <thomasbruckmaier@web.de>
2020-03-27 12:48:00 +0100

    Fixed tests

--- tests/database/factories/OptionFactory.php
+++ tests/database/factories/OptionFactory.php
@@ -4,7 +4,6 @@ use Corcel\Model\Option;
 
 $factory->define(Option::class, function (Faker\Generator $faker) {
     return [
-        'option_id' => $faker->numberBetween(1, 100),
         'option_name' => $faker->word,
         'option_value' => $faker->sentence(),
         'autoload' => 'yes',
